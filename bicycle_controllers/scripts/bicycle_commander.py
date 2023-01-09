Updated bicycle comander, updated bicycle_commander, and fixed sign error in lqr_controller to make commanded steering angle consistent with baseline whipple model

bicycle_controllers/scripts/bicycle_commander.py
@@ -1,30 +1,58 @@
 #!/usr/bin/env python3
 
-# Commands the bicycle
+""" Utility to command the bicycle. Can run continuously or issue a single command 
+depending on the arguments 
+
+--sine (specify frequency)
+--ramp (specify rate) (default is step)
+
+--continuous
+--speed 
+--steer_angle
+--heading 
+--roll 
+""" 
 
 import roslib
 import rospy
 import time
 from bicycle_msgs.msg import BicycleCmd
 from std_msgs.msg import Header
+import sys
+import argparse
+import numpy as np
 
 if __name__ == '__main__':
+
   rospy.init_node('bicycle_commander')
+  d0 = rospy.get_param("bicycle/initial_conditions/delta", 0)
+  v0 = rospy.get_param("bicycle/initial_conditions/vel", 0)
+
+  parser = argparse.ArgumentParser(description="bicycle_commander node. Default is step, but can command ramp or sine instead")
+  parser.add_argument('-v', '--speed', dest="speed", default=v0, help="Desired speed (v) in m/s")
+  parser.add_argument('-d', '--steer_angle', dest="delta", default=d0, help="Desired steer_angle (d) in deg")
+  parser.add_argument('-p', '--heading', dest="heading", default=0, help="Desired heading (p) in deg")
+
+  # TODO - implement these
+  parser.add_argument('-r', '--ramp', dest="ramp", default=0, help="Implement ramp for [v,d,p]. Provide ramp slope.")
+  parser.add_argument('-s', '--sine', dest="sine", default=0, help="Implement sine for [v,d,p]. Provide frequency (Hz).")
+
+  args = parser.parse_args()
+  print(args)
 
   command_pub = rospy.Publisher("bicycle/command", BicycleCmd, queue_size=1)
 
   cmd = BicycleCmd()
-  cmd.desired_steer_angle = 0
-  cmd.desired_speed = 4
+  cmd.desired_steer_angle = np.radians(float(args.delta))
+  cmd.desired_speed = float(args.speed)
 
-  rate = rospy.Rate(1) # Hz
+  rate = rospy.Rate(10) # Hz
 
   while not rospy.is_shutdown():
-
     try:
       h = Header()
       h.stamp = rospy.Time.now()
-      command_pub.header = h
+      cmd.header = h
       command_pub.publish(cmd)
       rate.sleep()
     except rospy.ROSTimeMovedBackwardsException:

bicycle_controllers/scripts/bicycle_controller.py
@@ -78,8 +78,10 @@ class BicycleController():
         # Actuator inputs
         self.Td = 0    # Steering torque
 
-        # High level steering commands
+        # High level steering commands. Initialize to maintain initial configuration. 
         self.commands = BicycleCmd()
+        self.commands.desired_steer_angle = np.radians(rospy.get_param("bicycle/initial_conditions/delta", 0))
+        self.commands.desired_speed = rospy.get_param("bicycle/initial_conditions/vel", 0)
 
         self.inputs = {}
 
@@ -95,11 +97,13 @@ class BicycleController():
         self.rate = rospy.Rate(1)
 
         self.n = 0
+        self.state = 'upright'
 
     def ReceiveCommand(self, msg):
         """ Receives BicycleCmd message """
         # Note - which values will actually be used depends on the particular controller. 
         self.commands = msg
+        #print("Received {:f} steer angle".format(msg.desired_steer_angle))
 
     def ReadJointStates(self, msg):
         """ Receives JointState messages and extracts the steering angle,
@@ -140,6 +144,7 @@ class BicycleController():
         # TODO - should we just store these in dictionary in first place? rather than in local variables 
         # and then packing up there?
         # TODO - can probably be replaced by BicycleStates msg, below
+        # NOTE - delta and deltadot are negative to align with whipple model convention
         self.inputs['phi']      = self.phi
         self.inputs['delta']    = -self.delta
         self.inputs['phidot']   = self.phidot.differentiate(self.phi, self.Ts)
@@ -164,6 +169,7 @@ class BicycleController():
         if self.n > 10:
             self.first_states_received = True
 
+
     def publish_path(self, event):
         # h = Header()
         # h.stamp = rospy.Time.now()
@@ -179,22 +185,45 @@ class BicycleController():
         
         self.path_pub.publish(self.path)
 
+    def kill_control(self):
+        """ Zero steering torque and command back wheel to come to stop. """
+        h = Header()
+        h.stamp = rospy.Time.now()
+
+        torque_cmd = JointState()
+        torque_cmd.header = h
+        torque_cmd.name = ["steering_joint"]
+        torque_cmd.effort = [0.0]
+
+        vel_cmd = JointState()
+        vel_cmd.header = h
+        vel_cmd.name = ["back_wheel_joint"]
+        vel_cmd.velocity = [0.0]
+
+        self.tau_pub.publish(torque_cmd)
+        self.vel_pub.publish(vel_cmd)
+
     def publish_control(self, event):
         if self.first_states_received:
             [torque_cmd, velocity_cmd, position_cmd] = self.controller.control(self.inputs, self.commands)
-            
-            if torque_cmd is not None:
-                self.tau_pub.publish(torque_cmd)
 
-            if velocity_cmd is not None:
-                self.vel_pub.publish(velocity_cmd)
+            # If we have fallen down, kill controller
+            if(np.abs(self.phi) > np.radians(70)):
+                self.kill_control()
 
-            if position_cmd is not None:
-                pass
+                if self.state == 'down':
+                    rospy.loginfo("Crash detected. state is {}. Ceasing controls".format(self.state))
+                    self.state = 'down'                    
+        
+            else:
+                if torque_cmd is not None:
+                    self.tau_pub.publish(torque_cmd)
 
-    def publish_states(self, event):
-        """ Publishes bicycle states """
+                if velocity_cmd is not None:
+                    self.vel_pub.publish(velocity_cmd)
 
+                if position_cmd is not None:
+                    pass
 
     def run(self):
         self.reset()

bicycle_controllers/scripts/lqr_controller.py
@@ -77,9 +77,13 @@ class LQR_Controller (Controller) :
             is of type sensor_msgs.JointState or None. 
         """
 
+        # Invert desired steer angle to align with Whipple model convention (+ is clockwise)
+        desired_steer_angle = cmd_msg.desired_steer_angle * -1
+        #print("Received {:f} steer angle".format(cmd_msg.desired_steer_angle))
+
         state = np.array([inputs['phi'], inputs['delta'], inputs['phidot'], inputs['deltadot']])
 
-        Td = self.kr*cmd_msg.desired_steer_angle - self.K.dot(state)
+        Td = self.kr*desired_steer_angle - self.K.dot(state)
         umax = 20
         Td = self.saturate(Td, umax, -umax)
         